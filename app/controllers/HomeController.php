487fb4828bcd1fe5724a8314df0006a5dc48fe48
Jeevanstar2 <jeevanthomasjaison01@algomau.ca>
2025-06-21 00:26:56 +0000

    created home controller to check if the session works when we run

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+class HomeController {
+    public function index() {
+        session_start();
+
+        if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
+            header("Location: index.php?action=login");
+            exit;
+        }
+
+        include __DIR__ . '/../views/home.php';
+    }
+}
