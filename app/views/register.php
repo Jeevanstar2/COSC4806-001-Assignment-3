f5b523532d91777c60d322d3b97b4ca71878ed5f
Jeevanstar2 <jeevanthomasjaison01@algomau.ca>
2025-06-21 00:30:47 +0000

    created a register.php for registering

---
+++ app/views/register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register</title>
+</head>
+<body>
+    <h2>Create a New Account</h2>
+
+    <?php if (isset($_GET['error'])): ?>
+        <p style="color: red;"><?php echo htmlspecialchars($_GET['error']); ?></p>
+    <?php endif; ?>
+
+    <form action="index.php?action=register" method="POST">
+        Username: <input type="text" name="username" required><br><br>
+        Password: <input type="password" name="password" required><br><br>
+        Confirm Password: <input type="password" name="confirm_password" required><br><br>
+        <button type="submit">Register</button>
+    </form>
+
+    <p><a href="index.php?action=login">Back to Login</a></p>
+</body>
+</html>
