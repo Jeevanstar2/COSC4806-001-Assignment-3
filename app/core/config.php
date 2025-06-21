3c938436e0b7b33a5035e95656fe43cec04732d2
Jeevanstar2 <jeevanthomasjaison01@algomau.ca>
2025-06-21 00:45:16 +0000

    updated config file

--- app/core/config.php
+++ app/core/config.php
@@ -2,7 +2,7 @@
 $host = "c0tme.h.filess.io";
 $dbname = "COSC4806001JS2_figurewhom";
 $username = "COSC4806001JS2_figurewhom";
-$password = "your_actual_password";
+$password = "$_ENV[password]";
 $port = "61000";
 
 try {
