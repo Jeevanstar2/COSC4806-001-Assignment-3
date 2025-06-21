9a51ca0b497fac8475941001b25b12c3985d18a0
Jeevanstar2 <jeevanthomasjaison01@algomau.ca>
2025-06-21 03:34:45 +0000

    style.css working!!!!!!

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
-<head><title>Rickroll</title></head>
+<head>
+    <title>Rickroll</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 <body>
-<h3>YEET</h3>
-<img src="https://www.icegif.com/wp-content/uploads/2023/01/icegif-162.gif" style="width:100%;max-width:600px;">
-<p><a href="index.php?action=logout">Logout</a></p>
+    <h3>YEET</h3>
+    <img src="https://www.icegif.com/wp-content/uploads/2023/01/icegif-162.gif" alt="Rickroll" style="width: 100%; max-width: 600px; display: block; margin: 0 auto;">
+    <p><a href="index.php?action=logout">Logout</a></p>
 </body>
 </html>
