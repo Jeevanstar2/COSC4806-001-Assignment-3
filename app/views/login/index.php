e2c6a9d2d641b095e675b4bd2c891059ff6db8b9
Jeevanstar2 <jeevanthomasjaison01@algomau.ca>
2025-06-21 00:40:31 +0000

    updated the index.php file in the view folder

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,26 +1,23 @@
-	<!DOCTYPE html>
-	<html>
-	<head>
-			<title>Login</title>
-			<style>
-					.error-msg {
-							color: red;
-							margin-left: 40px;
-					}
-			</style>
-	</head>
-	<body>
-			<h2>Login Page</h2>
-			<form action="index.php?action=login" method="POST">
-					Username: <input type="text" name="username" required><br><br>
-					Password: <input type="password" name="password" required><br><br>
+<!DOCTYPE html>
+<html>
+<head>
+		<title>Login</title>
+		<link rel="stylesheet" href="style.css">
+</head>
+<body>
+		<h2>Login Page</h2>
 
-					<?php if (isset($_GET['error'])): ?>
-							<p class="error-msg"><?php echo htmlspecialchars($_GET['error']); ?></p>
-					<?php endif; ?>
+		<form action="index.php?action=login" method="POST">
+				Username: <input type="text" name="username" required><br><br>
+				Password: <input type="password" name="password" required><br><br>
 
-					<input type="submit" value="Login">
-			</form>
-			<p>Don't have an account? <a href="index.php?action=register">Create a new account</a></p>
-	</body>
-	</html>
+				<?php if (isset($_GET['error'])): ?>
+						<p class="error-msg"><?php echo htmlspecialchars($_GET['error']); ?></p>
+				<?php endif; ?>
+
+				<input type="submit" value="Login">
+		</form>
+
+		<p>Don't have an account? <a href="index.php?action=register">Create a new account</a></p>
+</body>
+</html>
